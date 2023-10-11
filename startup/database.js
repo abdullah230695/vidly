82960b373fb6fae9ec91b5e28c21867b590da18a
shalla <abdullah2306@gmail.com>
2023-10-11 21:42:51 +0530

    mongo dp path updated

--- startup/database.js
+++ startup/database.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 module.exports = function connectMangoose() {
   const dbPath = process.env.dbPath;
-  mongoose.connect(dbPath).then(() => {
-    winston.info(`database connected : ${dbPath}`);
+  mongoose.connect(dbPath).then((v) => {
+    winston.info(`database connected`);
   });
 };
